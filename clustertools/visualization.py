2b8d1b1a301b72711ac6d1b9ad513a8da0593362
Christoph Lassner <mail@christophlassner.de>
2017-02-09 13:20:20 +0100

    Visualization more efficient (thx to Thomas).

--- clustertools/visualization.py
+++ clustertools/visualization.py
@@ -8,14 +8,18 @@ def apply_colormap(image, vmin=None, vmax=None, cmap='viridis'):
     """Apply a matplotlib colormap to an image."""
     image = image.astype("float64")
     # Normalization.
-    imin, imax = np.min(image), np.max(image)
     if vmin is not None:
         imin = float(vmin)
+    else:
+        imin = np.min(image)
     if vmax is not None:
         imax = float(vmax)
+    else:
+        imax = np.max(image)
     image -= imin
     image /= (imax - imin)
     # Visualization.
     cmap_ = plt.get_cmap(cmap)
     vis = cmap_(image, bytes=True)
     return vis
+
