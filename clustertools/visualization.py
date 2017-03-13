73c54811ceb6687d7eac40c17ea575fd31a5beb9
Christoph Lassner <mail@christophlassner.de>
2017-03-13 22:51:46 +0100

    Made vmin and vmax more robust.

--- clustertools/visualization.py
+++ clustertools/visualization.py
@@ -1,4 +1,5 @@
 """Visualization tools independent of OpenCV and available headless."""
+import sys
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
@@ -6,13 +7,15 @@ import numpy as np
 
 def apply_colormap(image, vmin=None, vmax=None, cmap='viridis'):
     """Apply a matplotlib colormap to an image."""
-    image = image.astype("float64")
+    image = image.astype("float64")  # Returns a copy.
     # Normalization.
     imin, imax = np.min(image), np.max(image)
     if vmin is not None:
         imin = float(vmin)
+        image = np.clip(image, vmin, sys.float_info.max)
     if vmax is not None:
         imax = float(vmax)
+        image = np.clip(image, -sys.float_info.max, vmax)
     image -= imin
     image /= (imax - imin)
     # Visualization.
